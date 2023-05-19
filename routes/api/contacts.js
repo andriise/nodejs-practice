cb774d7849340b8061fb298d0b7b7985f8203cf8
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-05-19 19:13:08 +0300

    add try/catch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,26 +5,56 @@ const router = express.Router();
 const contactsService = require("../../models/contacts");
 
 router.get("/", async (req, res, next) => {
-  const result = await contactsService.listContacts();
-  res.json(result);
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const result = await contactsService.getContactById();
-  res.json(result);
+  try {
+    const result = await contactsService.getContactById();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    res.json({ message: "template message" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const result = await contactsService.removeContact();
-  res.json(result);
+  try {
+    const result = await contactsService.removeContact();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    res.json({ message: "template message" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 module.exports = router;
