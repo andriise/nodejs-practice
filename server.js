e6ab7933e93ff314c6ef299de37c44c46eadae5c
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-05-26 22:43:37 +0300

    copying from node_1 + connect to database

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// sUorejdQhpibjd9B
+
+const DB_HOST =
+  "mongodb+srv://Andrii:sUorejdQhpibjd9B@cluster0.dzdy5b0.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
